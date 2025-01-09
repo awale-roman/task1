3bf6b49cf9542242860b3480815412b200602bb9
awale-roman <romanawale7@gmail.com>
2025-01-09 13:42:18 +0545

    navbar slicing

---
+++ src/helpers/staticData.js
@@ -0,0 +1,18 @@
+export const languages = [
+  "python",
+  "html",
+  "javascript",
+  "java",
+  "c++",
+  "rust",
+  "php",
+];
+
+export const ideThemes = [
+  "Github Light",
+  "Github Dark",
+  "VScode Light",
+  "VScode Dark",
+  "Material",
+  "OneDark",
+];
